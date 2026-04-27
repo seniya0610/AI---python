5c992396cc5a1551b2eba389d864cdc8c3c2ac33
Seniya Naeem <seniyanaeem@gmail.com>
2026-04-28 04:49:06 +0500

    Refactor A* algorithm into Environment and AStarAgent classes

--- lab5/Astar.py
+++ lab5/Astar.py
@@ -1,46 +1,63 @@
-graph = {
-    'A': [['B', 4], ['C', 3]],
-    'B': [['E', 12], ['F', 5]],
-    'C': [['D', 7], ['E', 10]],
-    'D': [['E', 2]],
-    'E': [['G', 5]],
-    'F': [['G', 16]],
-    'G': []
-}
+class Environment:
+    def __init__(self):
+        self.graph = {
+            'A': [['B', 4], ['C', 3]],
+            'B': [['E', 12], ['F', 5]],
+            'C': [['D', 7], ['E', 10]],
+            'D': [['E', 2]],
+            'E': [['G', 5]],
+            'F': [['G', 16]],
+            'G': []
+        }
+        self.heuristic = {
+            'A': 14, 'B': 12, 'C': 11,
+            'D': 6,  'E': 4,  'F': 11, 'G': 0
+        }
 
-heuristic = {
-    'A': 14, 'B': 12, 'C': 11,
-    'D': 6,  'E': 4,  'F': 11, 'G': 0
-}
+    def get_neighbors(self, node):
+        return self.graph[node]
 
-def a_star(start, goal):
-    open_list = [(heuristic[start], start, [start], 0)]
-    #             f=g+h,           node,  path,     g
-    visited = []
+    def get_heuristic(self, node):
+        return self.heuristic[node]
 
-    while len(open_list) > 0:
-        open_list.sort()             # sorts by f = g + h
-        f, node, path, g = open_list.pop(0)
 
-        if node in visited:
-            continue
+class AStarAgent:
+    def __init__(self, environment):
+        self.env = environment
 
-        visited.append(node)
-        print("Visiting:", node, " cost so far:", g)
+    def search(self, start, goal):
+        open_list = [(self.env.get_heuristic(start), start, [start], 0)]
+        #             f=g+h                           node   path    g
+        visited = []
 
-        if node == goal:
-            print("Path:", path)
-            print("Total cost:", g)
-            return
+        while open_list:
+            open_list.sort()
+            f, node, path, g = open_list.pop(0)
 
-        for pair in graph[node]:
-            neighbor = pair[0]
-            edge_cost = pair[1]
-            if neighbor not in visited:
-                new_g = g + edge_cost
-                new_f = new_g + heuristic[neighbor]
-                open_list.append((new_f, neighbor, path + [neighbor], new_g))
+            if node in visited:
+                continue
+            visited.append(node)
+            print(f"Visiting: {node} | Cost so far: {g}")
 
-    print("No path found")
+            if node == goal:
+                print(f"Path: {' -> '.join(path)}")
+                print(f"Total cost: {g}")
+                return path
 
-a_star('A', 'G')
+            for neighbor, edge_cost in self.env.get_neighbors(node):
+                if neighbor not in visited:
+                    new_g = g + edge_cost
+                    new_f = new_g + self.env.get_heuristic(neighbor)
+                    open_list.append((new_f, neighbor, path + [neighbor], new_g))
+
+        print("No path found")
+        return None
+
+
+def run_agent(start, goal):
+    env = Environment()
+    agent = AStarAgent(env)
+    agent.search(start, goal)
+
+
+run_agent('A', 'G')
